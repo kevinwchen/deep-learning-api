124adc5eed5c3600964877c2707285c3a0b6ffce
Kevin Chen <47770053+kevinwchen@users.noreply.github.com>
2023-04-06 23:45:38 +1000

    Add translation model to api

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# deep-learning-api
+# Translation API
+
+This is a web server app that can translate text based on a user input. The input is taken in through a post request, and the result extracted from a database once the translation has finished running.
--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI, BackgroundTasks
 from pydantic import BaseModel, validator
+from models import TranslationModel
 import tasks
 
 app = FastAPI()
+
+langueages = ["English", "French", "German", "Romanian"]
+
+class Translation(BaseModel):
+    text: str
+    base_lang: str
+    final_lang: str
+
+    @validator('base_lang', 'final_lang')
+    def valid_lang(cls, lang):
+        if lang not in langueages:
+            raise ValueError("Invalid language")
+        return lang
 
 # Route 1:
 # Test if working
@@ -15,8 +29,25 @@ def get_root():
 # Route 2: /translate
 # Take in a translation request, store it the database
 # Return a transaction id
+@app.post("/translate")
+def post_translation(t: Translation, background_tasks: BackgroundTasks):
+    # Store the translation
+    # Run the translation in the background
+    t_id = tasks.store_translation(t)
+    background_tasks.add_task(tasks.run_translation, t_id)
+    return {"task_id": t_id}
+
 
 # Route 3: /results
 # Take in a translation id
 # Return the translated text
+@app.get("/results")
+def get_translation(t_id: int):
+    model = TranslationModel.get_by_id(t_id)
+
+    translation = model.translation
+    if translation is None:
+        translation = "Processing, check back later"
+
+    return translation
 
